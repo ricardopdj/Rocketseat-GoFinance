2db225b54811bd65848d580e08ba7fb268f4d67b
Ricardo Pareja <pareja.r@gmail.com>
2021-12-12 22:19:18 -0300

    Add ts and custom fonts

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+import { Container, Title } from "./styles";
+
+const Dashboard: React.FC = () => {
+  return (
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+  );
+};
+
+export default Dashboard;
