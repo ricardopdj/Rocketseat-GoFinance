d03cc3025798431a6a84dc73d3f5c18aa5c4b646
Ricardo Pareja <pareja.r@gmail.com>
2021-12-27 10:39:34 -0300

    Finish cards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,59 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import HighlightCard from "../../components/HighlightCard";
 
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  HighlightCards,
+  Icon,
+  Photo,
+  User,
+  UserGreeting,
+  UserInfo,
+  UserName,
+  UserWrapper,
+} from "./styles";
 
 const Dashboard: React.FC = () => {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/2244129?v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Olá</UserGreeting>
+              <UserName>Ricardo</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCards>
+        <HighlightCard
+          title="Entradas"
+          amount="R$ 17.000,00"
+          lastTransaction="ültima entrada dia 13 de abril"
+          type="up"
+        />
+        <HighlightCard
+          title="Saídas"
+          amount="R$ 17.000,00"
+          lastTransaction="ültima entrada dia 13 de abril"
+          type="down"
+        />
+        <HighlightCard
+          title="Total"
+          amount="R$ 17.000,00"
+          lastTransaction="ültima entrada dia 13 de abril"
+          type="total"
+        />
+      </HighlightCards>
     </Container>
   );
 };
